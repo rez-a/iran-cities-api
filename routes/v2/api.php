e5e2813a32f2983e0d6f35c68b27f2ada74cf1d0
rez-a <reza.hasani11177@gmail.com>
2026-09-04 13:13:51 +0330

    phase-2 is completed

--- routes/v2/api.php
+++ routes/v2/api.php
@@ -6,13 +6,13 @@ use Iran\Controllers\V2\ProvincesController;
 use Iran\Controllers\V2\AuthController;
 
 $routes = [
-    ['method' => 'GET', 'path' => '/api/v2/cities', 'handler' => [CitiesController::class, 'index'] , 'auth' => true , "role"=>"user"],
-    ['method' => 'GET', 'path' => '/api/v2/cities/{id}', 'handler' => [CitiesController::class, 'getById'] , 'auth' => true , "role"=>"user"],
+    ['method' => 'GET', 'path' => '/api/v2/cities', 'handler' => [CitiesController::class, 'index'] , 'auth' => true , "role"=>null],
+    ['method' => 'GET', 'path' => '/api/v2/cities/{id}', 'handler' => [CitiesController::class, 'getById'] , 'auth' => true , "role"=>null],
     ['method' => 'POST', 'path' => '/api/v2/cities', 'handler' => [CitiesController::class, 'create'] , 'auth' => true , "role"=>"admin"],
     ['method' => 'PUT', 'path' => '/api/v2/cities/{id}', 'handler' => [CitiesController::class, 'update'] , 'auth' => true , "role"=>"admin"],
     ['method' => 'DELETE', 'path' => '/api/v2/cities/{id}', 'handler' => [CitiesController::class, 'delete'] , 'auth' => true  , "role"=>"admin"],
-    ['method' => 'GET', 'path' => '/api/v2/provinces', 'handler' => [ProvincesController::class, 'index'] , 'auth' => true , "role"=>"user"],
-    ['method' => 'GET', 'path' => '/api/v2/provinces/{id}', 'handler' => [ProvincesController::class, 'getById'] , 'auth' => true , "role"=>"user"],
+    ['method' => 'GET', 'path' => '/api/v2/provinces', 'handler' => [ProvincesController::class, 'index'] , 'auth' => true , "role"=>null],
+    ['method' => 'GET', 'path' => '/api/v2/provinces/{id}', 'handler' => [ProvincesController::class, 'getById'] , 'auth' => true , "role"=>null],
     ['method' => 'POST', 'path' => '/api/v2/provinces', 'handler' => [ProvincesController::class, 'create'] , 'auth' => true , "role"=>"admin"],
     ['method' => 'PUT', 'path' => '/api/v2/provinces/{id}', 'handler' => [ProvincesController::class, 'update'] , 'auth' => true , "role"=>"admin"],
     ['method' => 'DELETE', 'path' => '/api/v2/provinces/{id}', 'handler' => [ProvincesController::class, 'delete'] , 'auth' => true , "role"=>"admin"],
